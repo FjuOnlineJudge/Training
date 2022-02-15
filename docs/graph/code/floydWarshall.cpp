02b67e9fdea94139763281f8587c5dd3029b9a3a
allem40306 <allem40306@gmail.com>
2022-02-15 21:39:54 +0800

    修改 floydWarshall 演算法程式碼

--- docs/graph/code/floydWarshall.cpp
+++ docs/graph/code/floydWarshall.cpp
@@ -1,4 +1,4 @@
 for (k = 0; k < n; k++)
     for (i = 0; i < n; i++)
         for (j = 0; j < n; j++)
-            w[i][j] = w[j][i] = min(w[i][j], max(w[i][k], w[k][j]));
+            w[i][j] = w[j][i] = min(w[i][j], w[i][k] + w[k][j]);
